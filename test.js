e9ec9400292628abb3ba5a3f22e71193d0f37887
dongkyusimaea <dongkyusim.aea@gmail.com>
2022-07-15 10:42:50 +0900

    test

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-for (let i = 1; i <= 50; i++) {
-  if (i % 2 == 0) {
-    alert(i);
+const even = () => {
+  let nums = [];
+
+  for (let i = 1; i <= 50; i++) {
+    if (i % 2 == 0) {
+      alert(i);
+    }
   }
-}
-return 50;
+  return nums;
+};
